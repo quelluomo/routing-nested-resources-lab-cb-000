5659884827fe49fe0a1aada682818f33fa1a4fae
Matteo Muscas <thatman.j@gmail.com>
2019-06-28 09:10:26 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,7 +7,7 @@ class SongsController < ApplicationController
         redirect_to artists_path, alert: "Artist not found."
       else
         @songs = @artist.songs
-      
+
       @songs = Song.all
     end
   end
