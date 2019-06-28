3719f7389b85532034c2bc4593c6b9cedc47efed
Matteo Muscas <thatman.j@gmail.com>
2019-06-28 10:46:22 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   #resources :artists
   #resources :songs
 
-  resources :artists do
+  resources :artists, only: [:show, :index] do
     resources :songs, only: [:show, :index]
   end
 
-  resources :songs
+  resources :songs, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'songs#index'
 
