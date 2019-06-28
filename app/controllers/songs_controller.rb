e891588f0a7ebdbaa17eb3708d5963b152f4e453
Matteo Muscas <thatman.j@gmail.com>
2019-06-28 10:40:29 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -19,7 +19,7 @@ class SongsController < ApplicationController
       if @artist.nil?
         redirect_to artists_songs_path, alert: "Song not found."
       else
-        @song = @artist.song.find(params[:id])
+        @song = @artist.song.find_by(id: id)
       end
     else
       @song = Song.find(params[:id])
