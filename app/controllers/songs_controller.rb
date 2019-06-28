9f71a868c2a2922423bcbf89d54bead746093eb5
Matteo Muscas <thatman.j@gmail.com>
2019-06-28 08:47:45 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,6 +3,7 @@ class SongsController < ApplicationController
     if params[:artist_id]
       @songs = Artist.find(params[:artist_id]).songs
     else
+      flash[:alert] = "Artist not found."
       @songs = Song.all
     end
   end
