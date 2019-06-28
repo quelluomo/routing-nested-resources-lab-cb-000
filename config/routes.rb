8ae84531aeb556990e257f992dfde776dc7feaff
Matteo Muscas <thatman.j@gmail.com>
2019-06-28 09:53:03 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   #resources :artists
   #resources :songs
 
-  resources :artists, only: [:show] do
+  resources :artists, only: [:show, :index] do
     resources :songs, only: [:show, :index]
   end
 
